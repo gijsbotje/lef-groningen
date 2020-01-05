20d21d687622e41fb2018ea87b899d4e470d5a0c
gijsbotje <gijs@afosto.io>
2020-01-06 00:21:59 +0100

    fix tags page

--- src/pages/tags/index.js
+++ src/pages/tags/index.js
@@ -1,8 +1,11 @@
-import React from 'react'
-import { kebabCase } from 'lodash'
-import Helmet from 'react-helmet'
-import { Link, graphql } from 'gatsby'
-import Layout from '../../components/Layout'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { kebabCase } from 'lodash';
+import Helmet from 'react-helmet';
+import { Link, graphql } from 'gatsby';
+import Container from '../../components/Container';
+import Section from '../../components/Section';
+import Typography from '@material-ui/core/Typography';
 
 const TagsPage = ({
   data: {
@@ -12,33 +15,34 @@ const TagsPage = ({
     },
   },
 }) => (
-  <Layout>
-    <section className="section">
+  <Container>
+    <Section>
       <Helmet title={`Tags | ${title}`} />
-      <div className="container content">
-        <div className="columns">
-          <div
-            className="column is-10 is-offset-1"
-            style={{ marginBottom: '6rem' }}
-          >
-            <h1 className="title is-size-2 is-bold-light">Tags</h1>
-            <ul className="taglist">
-              {group.map(tag => (
-                <li key={tag.fieldValue}>
-                  <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
-                    {tag.fieldValue} ({tag.totalCount})
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </section>
-  </Layout>
-)
+      <Typography variant="h3" component="h1">
+        Tags
+      </Typography>
+      <ul className="taglist">
+        {group.map(tag => (
+          <li key={tag.fieldValue}>
+            <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
+              {tag.fieldValue} ({tag.totalCount})
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </Section>
+  </Container>
+);
 
-export default TagsPage
+TagsPage.propTypes = {
+  data: PropTypes.object,
+};
+
+TagsPage.defaultProps = {
+  data: undefined,
+};
+
+export default TagsPage;
 
 export const tagPageQuery = graphql`
   query TagsQuery {
@@ -54,4 +58,4 @@ export const tagPageQuery = graphql`
       }
     }
   }
-`
+`;
